45edd0911652e28b5ba8eeaf7ef51e25d63fb628
Jon Rice <revyolution1120@gmail.com>
2020-01-11 09:53:50 -0800

    setting up project with homepage of app and account page.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <h1>Start of App</h1>
+    </div>
+)
+
+export default HomePage;
