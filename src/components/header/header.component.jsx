e089b00e56253b200b95bb00f13005e4eddba6e4
Jon Rice <revyolution1120@gmail.com>
2020-01-11 16:10:05 -0800

    Added logos for header and styled header.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as HomeLogo } from '../../assets/pokemon.svg';
+import { ReactComponent as VideoGameLogo } from '../../assets/gameboy.svg';
+import { ReactComponent as CardLogo } from '../../assets/camera.svg';
+import { ReactComponent as AboutLogo } from '../../assets/pokemon-trainer.svg';
+
 
 import './header.styles.scss';
 
 const Header = () => (
     <div className='header'>
+        <Link className='home-logo-container' to='/'>
+            <HomeLogo className='logo' />
+            Home
+        </Link>
         <div className='options'>
-            <Link className='option' to='/'>
-                Home
-            </Link>
             <Link className='option' to='/vghome'>
-                VG
+                <VideoGameLogo />
+                Video Games
             </Link>
             <Link className='option' to='/tcghome'>
-                TCG 
+                <CardLogo />
+                Trading Card Game 
             </Link>
             <Link className='option' to='/about'>
+                <AboutLogo />
                 About
             </Link>
         </div>
